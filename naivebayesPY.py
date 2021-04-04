60c013a52b1ccb081f933bda6f91b89a2140f4c8
LEE WEI SHAN <weishan_lee@yahoo.com>
2021-04-04 09:25:04 +0800

    Update naivebayesPY.py
    This should work!

--- naivebayesPY.py
+++ naivebayesPY.py
@@ -2,44 +2,29 @@
 # -*- coding: utf-8 -*-
 """
 @author: Nigel
+Modified by Wei shan Lee on Apr. 4, 2021.
 """
 
 import numpy as np
 
 def naivebayesPY(x, y):
-    # function [pos,neg] = naivebayesPY(x,y);
-    #
-    # Computation of P(Y)
-    # Input:
-    # x : n input vectors of d dimensions (dxn)
-    # y : n labels (-1 or +1) (1xn)
-    #
-    # Output:
-    # pos: probability p(y=1)
-    # neg: probability p(y=-1)
-    #
-    
-    # Convertng input matrix x and y into NumPy matrix
-    # input x and y should be in the form: 'a b c d...; e f g h...; i j k l...'
-    X = np.matrix(x)
-    Y = np.matrix(y)
-    
-    # Pre-configuring the size of matrix X
-    d, n_orig = X.shape
-    
-    # Pre-constructing a matrix of all-ones (dx2)
-    X0 = np.ones((d,2))
-    Y0 = np.matrix('-1, 1')
-    
-    # add one all-ones positive and negative example
-    Xnew = np.hstack((X, X0)) #stack arrays in sequence horizontally (column-wise)
-    Ynew = np.hstack((Y, Y0))
+    """
+    naivebayesPY(Y) returns [pos,neg]
 
-    # Re-configuring the size of matrix Xnew
-    d, n = Xnew.shape
+    Computation of P(Y)
+    Input:
+        X : n input vectors of d dimensions (nxd)
+        Y : n labels (-1 or +1) (nx1)
+
+    Output:
+        pos: probability p(y=1)
+        neg: probability p(y=-1)
+    """
     
-    ## fill in code here
-    pos = np.count_nonzero(Y == -1) / n_orig
-    neg = np.count_nonzero(Y == 1) / n_orig
+    # add one positive and negative example to avoid division by zero ("plus-one smoothing")
+    Y = np.concatenate([Y, [-1,1]])
+    n = len(Y)
     
+    pos = np.count_nonzero(Y == 1) / n
+    neg = np.count_nonzero(Y == -1) / n
     return pos,neg
